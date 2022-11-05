7db6ce6e6354633d99e11e6525651f2f7509b3b5
Judadego <judadego@gmail.com>
2022-11-05 00:28:14 -0500

    registro_part2

--- pets_front/src/pages/Home.js
+++ pets_front/src/pages/Home.js
@@ -1,27 +1,137 @@
-import React from 'react'
-
+import React from "react";
+import "../css/home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <div>
-                <section className="py-5 text-center container">
-                    <div className="row py-lg-5">
-                        <div className="col-lg-6 col-md-8 mx-auto">
-                            <h1 className="fw-light">Vista home</h1>
-                            <p className="lead text-muted">Something short and leading about the collection below—its contents, the
-                                creator,
-                                etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.
-                                Something short and leading about the collection below—its contents, the
-                                creator,
-                                etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                        </div>
-
-                    </div>
-                </section>
-            </div>
+  return (
+    <main>
+      <div class=" overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+          <h1
+            class="display-4 fw-normal"
+            _msthash="706186"
+            _msttexthash="491595"
+          >
+            Home
+          </h1>
+          <p class="lead fw-normal" _msthash="671424" _msttexthash="11293334">
+            Y un subtítulo aún más ingenioso para arrancar. Impulse sus
+            esfuerzos de marketing con este ejemplo basado en las páginas de
+            marketing de Apple.
+          </p>
+          <a
+            class="btn btn-outline-secondary"
+            href="/"
+            _msthash="670176"
+            _msttexthash="223262"
+          >
+            Próximamente
+          </a>
         </div>
-    )
-}
+        <div class="product-device shadow-sm d-none d-md-block"></div>
+        <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+      </div>
 
-export default Home
+      <div class="d-md-flex flex-md-equal w-100 my-md-10 ps-md-3">
+        <div class="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 py-3">
+            <h2 class="display-5" _msthash="969761" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="928226" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-light shadow-sm mx-auto" id="style1"></div>
+        </div>
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 p-3">
+            <h2 class="display-5" _msthash="970047" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="928499" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-dark shadow-sm mx-auto" id="style2"></div>
+        </div>
+      </div>
+
+      <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 p-3">
+            <h2 class="display-5" _msthash="970125" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="928577" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-dark shadow-sm mx-auto" id="style3"></div>
+        </div>
+        <div class="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 py-3">
+            <h2 class="display-5" _msthash="970411" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="928850" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-light shadow-sm mx-auto" id="style4"></div>
+        </div>
+      </div>
+
+      <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 p-3">
+            <h2 class="display-5" _msthash="970489" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="928928" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-body shadow-sm mx-auto" id="style5"></div>
+        </div>
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 py-3">
+            <h2 class="display-5" _msthash="970775" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="929201" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-body shadow-sm mx-auto" id="style6"></div>
+        </div>
+      </div>
+
+      <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 p-3">
+            <h2 class="display-5" _msthash="970853" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="929279" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-body shadow-sm mx-auto" id="style7"></div>
+        </div>
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 py-3">
+            <h2 class="display-5" _msthash="971139" _msttexthash="187317">
+              Otro titular
+            </h2>
+            <p class="lead" _msthash="929552" _msttexthash="880126">
+              Y un subtítulo aún más ingenioso.
+            </p>
+          </div>
+          <div class="bg-body shadow-sm mx-auto" id="style8"></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
